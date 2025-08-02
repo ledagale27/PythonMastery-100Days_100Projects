18377ff7883d18b7a67b088c6a06a3a8e40ba123
ledagale27 <ledagale@gmail.com>
2025-08-02 23:30:54 +0100

    Message generator

---
+++ 002-message_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Welcome Message Generator
+
+#Step 1: Ask for user details
+name = input("What's your name?")
+hobby = input("What's your favourite hobby?")
+
+#Step 2: Generate a personalized welcome message
+print("\n --- Welcome Message ---")
+print("Hello,{name} !".format(name =name))
+print(f"Welcome to the world of Python Programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to build something amazing today.")
+
+
