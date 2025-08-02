51c21c71f989000ff5e318a000ba544ffdab000a
ledagale27 <ledagale@gmail.com>
2025-08-02 23:54:15 +0100

    Personalized Greetings

---
+++ 003-personalized_greeting.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Greeting Program
+
+#Step 1: To ask for user details
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+color = input("What's your favourite color? ")
+
+#Step 2: Generatea personalized greeting message
+print("\n---Personalized Greeting---")
+print(f'Hello, {name}!')
+print(f'You are {age} years old and {color} is a beautiful color!')
+print(f"You're now ready to start your Python adventure.")
+
+
