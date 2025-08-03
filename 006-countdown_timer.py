21bda81c4b4fb4e4fc4c8c8c27a94cbbcee5f490
ledagale27 <ledagale@gmail.com>
2025-08-03 22:55:28 +0100

    Countdown Timer

---
+++ 006-countdown_timer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Countdown timer
+
+import time
+
+#Step 1: Get user input for countdown start
+start = int(input("Enter the number to start the countdown from: "))
+
+#Step 2: Countdown using a while loop
+print("\n--- Countdown Begins ---")
+while start > 0:
+    print(start)
+    time.sleep(1)
+    start -= 1
+
+#Step 3: Print "Countdown Complete!"
+print("\n --- Countdown Complete! ---")
+
