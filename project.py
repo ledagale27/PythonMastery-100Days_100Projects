b0ffa37997208560f8273b317b0efe909445641d
ledagale27 <ledagale@gmail.com>
2025-08-03 22:05:55 +0100

    Calculator

--- project.py
+++ project.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-#Simple Calculator - Basic option 1
-
-#Step 1: Get user input for two numbers
-#number_1 = float(input("Enter the first number: "))
-#number_2 = float(input("Enter the second number: "))
-
-#Step 2: Perform arithmetic operation
-#addition = number_1 + number_2
-#substraction = number_1 - number_2
-#multiplication = number_1 * number_2
-#division = number_1 / number_2 if number_2 != 0 else "Cannot divide by zero"
-
-#Step 3: Display the results
-#print("\n --- Calculator Results ---")
-#print(f'Addition: {number_1} + {number_2} = {addition}')
-#print(f'Substraction: {number_1} - {number_2} = {substraction}')
-#print(f'Multiplication: {number_1} x {number_2} = {multiplication}')
-#print(f'Division: {number_1} / {number_2} = {division}')
-
-#Calculator Option 2
-
 def calculator():
     while True:
         print("\n--- Simple Calculator ---")
